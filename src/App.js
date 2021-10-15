3e6b09184e4fbe7550100c1f9071a1dc71780659
Suraj Kumar <suraj.k@collaberus.com>
2021-10-15 08:34:30 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Component/Navbar'
+import TextUtils from './Component/TextUtils';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#0B0B45';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Navbar title="Developer" name="About-us" mode={mode} toggleMode={toggleMode} />
+    <TextUtils title="Enter your text here for easy modifying" mode={mode} />
+   </>
   );
 }
 
